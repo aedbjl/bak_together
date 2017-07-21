e700fa40dfd7b294ea010ddc8178f918a32a2658
Rand God <iii-user@iii-userde-MacBook-4.local>
2017-07-22 00:45:38 +0800

    v1

---
+++ together/openGroupVC.swift
@@ -0,0 +1,37 @@
+//
+//  openGroupVC.swift
+//  together
+//
+//  Created by Seven Tsai on 2017/7/17.
+//  Copyright © 2017年 Seven Tsai. All rights reserved.
+//
+
+import UIKit
+
+class openGroupVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        //dev
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
